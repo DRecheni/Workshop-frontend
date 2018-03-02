ca33dbb42452fd0933dd3a7f321aa1de5491a5cc
MEMGIT <cwalzel@memas.de>
2018-03-02 17:42:12 +0100

    made some initial setup for local devlopment

--- facturee-dashboard/app/main/constants/config-const.js
+++ facturee-dashboard/app/main/constants/config-const.js
@@ -5,6 +5,9 @@ angular.module('main')
   // gulp environment: injects environment vars
   ENV: {
     /*inject-env*/
+    'serverURL': 'http://localhost:1337/1',
+    'appId': 'myAppId',
+    'JSKEy': 'Z4BaU4AU37Fari86Kra1osNa9czGhmRhAe6fXPqS'
     /*endinject*/
   },
 
--- facturee-dashboard/app/main/main.js
+++ facturee-dashboard/app/main/main.js
@@ -1,11 +1,17 @@
 'use strict';
+
+
+
 angular.module('main', [
   'ionic',
   'ngCordova',
   'ui.router',
+  'ngParse'
   // TODO: load other modules selected during generation
 ])
 .config(function ($stateProvider, $urlRouterProvider) {
+
+
 
   // ROUTING with ui.router
   $urlRouterProvider.otherwise('/main');
@@ -17,4 +23,18 @@ angular.module('main', [
       // templateUrl: 'main/templates/<someTemplate>.html',
       // controller: 'SomeCtrl as ctrl'
     });
+})
+.config(function (ParseProvider,Config, $windowProvider) {
+
+  const MY_PARSE_APP_ID = Config.ENV.appId;
+  const MY_PARSE_JS_KEY = Config.ENV.JSKey;
+  const SERVER_URL = Config.ENV.serverURL;
+
+  var $window = $windowProvider.$get();
+
+
+  $window.Parse.serverURL = SERVER_URL;
+  ParseProvider.initialize(MY_PARSE_APP_ID,MY_PARSE_JS_KEY);
+
+
 });
